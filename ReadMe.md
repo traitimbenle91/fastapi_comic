bbd00901b914c9276efff5a12da7fb39e25c7c28
tran manh <traitimbenle91@gmail.com>
2026-03-04 22:58:11 +0700

    update new web nettruyen

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+#Install packages
+$pip install -r requirements.txt
+
+#run app
+$uvicorn main:app --reload
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from fastapi import FastAPI, HTTPException
 from typing import List, Optional
+from urllib.parse import urlsplit
 import re
 import json
 
@@ -15,29 +16,50 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
 
-root_url = "https://aquastarsleep.co.uk"
+root_url = "https://nettruyenviet1.com"
+            
+
 reflex_url = "http://10.0.2.2:8000/api/v1/manga"
 class Story:
-    def __init__(self, name = None, img_cover = None, story_link = None, chapter = None, chapter_link = None, chapter_release = None, number_view = None, star_rating = None):
+    def __init__(self, name):
         self.name = name
-        self.img_cover = img_cover
-        self.story_link = story_link
-        self.chapter = chapter
-        self.chapter_link = chapter_link
-        self.chapter_release = chapter_release
-        self.number_view = number_view
-        self.star_rating = star_rating
-        self.kind_story = None
-        self.manufacturer = None
-        self.dest = None
-        self.chapters = []
+        self.link = ""
+        self.cover = ""
+        self.views = ""
+        self.comments = ""
+        self.hearts = ""
+        self.chapter_name = ""
+        self.chapter_link = ""
+        self.chapter_release = ""
+
+        self.author = ""
+        self.status = ""
+        self.kind = ""
+        self.rating = 3.0
+        self.rating_count = ""
+
+        # self.number_view = ""
+        # self.star_rating = 5.0
+        # self.kind_story = ""
+        # self.manufacturer = ""
+        # self.dest = ""
+        # self.chapters = []
 
     def __str__(self):
-        return f"{self.name}||{self.chapter}|| {self.chapter_release}||{self.img_cover}||{self.story_link}||{self.number_view}||{self.star_rating}"
+        return "====================================================== \n" + \
+            self.name + "\n" + \
+            self.link + "\n" + \
+            self.cover + "\n" + \
+            self.views + "\n" + \
+            self.comments + "\n" + \
+            self.hearts + "\n" + \
+            self.chapter_name + "\n" + \
+            self.chapter_link + "\n" + \
+            self.chapter_release + "\n"
+        
 
 def crawl_story_names(page_number: int) -> Optional[list[Story]]:
-    # "https://aquastarsleep.co.uk/tim-kiem?r=newly-updated&page=1
-    url = f"{root_url}/tim-kiem?r=newly-updated&page={page_number}"
+    url = f"{root_url}/?page={page_number}"
     
     try:
         response = requests.get(url = url, headers=headers, timeout=15)
@@ -48,39 +70,45 @@ def crawl_story_names(page_number: int) -> Optional[list[Story]]:
 
         storys = {}
 
-        #Get Story name, link, image_cover
-        elements = soup.find_all('div', class_ = 'p-thumb flex-shrink-0')
+        content = soup.find('div', class_ = 'center-side col-md-8')
+
+        # find all item content which contain manga item
+        elements = content.find_all('div', class_ = 'item')
         
         if not elements:
             return []
         for e in elements:
-            link = f"{e.a.get('href')}"
-            name = e.a.get('title')
-            img_cover = e.a.span.img.get('data-src')
-            storys[name] = Story(name, img_cover, link)
+            name = e.figure.div.a.get('title')
+            storys[name] = Story(name)
+            
+            url = f"{e.figure.div.a.get('href')}"
+            # parsed = urlparse(url)
+            # path = parsed.path.lstrip('/')
+            storys[name].link = urlsplit(url).path
 
-        #Get Story_name, number_view, 
-        elements = soup.find_all('div', class_ = 'p-content flex-grow-1')
+            storys[name].cover = e.figure.div.a.img.get('data-retries')
+            storys[name].views = e.figure.div.find('i', class_ = "fa fa-eye").next_sibling.strip()
+            storys[name].comments = e.figure.div.find('i', class_ = "fa fa-comment").next_sibling.strip()
+            storys[name].hearts = e.figure.div.find('i', class_ = "fa fa-heart").next_sibling.strip()
+            
+            chapterContent = e.figure.figcaption.find_all("li", class_ = "chapter clearfix")
+            storys[name].chapter_name = chapterContent[0].a.get('title')
+            storys[name].chapter_link = chapterContent[0].a.get('href')
+            storys[name].chapter_release = chapterContent[0].i.get_text()
 
-        if not elements:
-            return []
+            # print(storys[name])
 
-        for e in elements:
-            name = e.h3.a.string
-            storys[name].chapter_link = f"{e.ul.li.a.get('href')}"
-            storys[name].chapter = e.ul.li.a.get('href').split("/")[-1].capitalize()
-            storys[name].chapter_release = e.ul.li.a.span.string
-            storys[name].number_view = e.find('span', class_="num-view").string
-            storys[name].star_rating = e.div.find_all('span')[2].string
+            
+            
+            # return storys.values()
     
-        # for s in storys.values():
-        #     print(s)
         return storys.values()
     except requests.exceptions.RequestException as e:
         print(f"There is an error when access {e}")
 
 
-# crawl_story_names(1)
+# storys = crawl_story_names(1)
+
 
 @app.get("/api/v1/manga/{page_number}")
 async def get_story_name(page_number: int):
@@ -99,18 +127,19 @@ async def get_story_name(page_number: int):
 
     for story in storys:
         objs.append({
-            "story name": story.name,
-            "chapter name": story.chapter,
-            "chapter link": story.chapter_link,
-            "chapter release": story.chapter_release,
-            "story link": story.story_link,
-            "link image": story.img_cover,
-            "number view": story.number_view,
-            "star rating": float(story.star_rating)
-
+            "name": story.name,
+            "link": story.link,
+            "cover": story.cover,
+            "views": story.views,
+            "comments": story.comments,
+            "hearts": story.hearts,
+            "lastChapter": story.chapter_name,
+            "lastChapterLink": story.chapter_link,
+            "lastChapterRelease": story.chapter_release
         })
     return objs
-
+    
+    
 class Chapter:
     def __init__(self, chapter_name, link_chapter, time, number_view):
         self.chapter_name = chapter_name
@@ -132,33 +161,33 @@ def crawl_list_chapters(story_name: str) -> Optional[Story]:
 
         soup = BeautifulSoup(response.content, 'html.parser')
         
-        story = Story()
-        story.kind_story = soup.find('div', class_="kind").img.string
-        story.manufacturer = soup.find('div', class_="kind").img.get("src").split("/")[-1].split(".")[0].upper()
-        story.dest = soup.find('div', class_="line-clamp html-content").get_text(separator="\n", strip=True)
-        
-        elements = soup.find_all('div', class_ = "l-chapter") 
-
-        if not elements:
-            return []
-        
+        story = Story("")
+        story.name = soup.find('div', class_="center-side col-md-8").article.h1.get_text()
+        story.cover = soup.find('div', class_="col-xs-4 col-image").img.get("src")
+        story.author = soup.find('li', class_="author row").find('p', class_ = "col-xs-8").get_text()
+        story.status = soup.find('li', class_="status row").find('p', class_ = "col-xs-8").get_text()
+        story.kind = soup.find('li', class_="kind row").find('p', class_ = "col-xs-8").get_text()
+        story.rating = soup.find('div', class_="mrt5 mrb10").find('span', attrs={'itemprop': 'ratingValue'}).text
+        story.rating_count = soup.find('div', class_="mrt5 mrb10").find('span', attrs={'itemprop': 'ratingCount'}).text
+                
         list_chapters = []
+        elements = soup.find(id="chapter_list").find_all('li', class_='row')
         
         for e in elements:        
-            chapter_name = e.a.get('title')
-            link_chapter = f"{e.a.get('href')}"
-            time = e.find_all('span')[0].string.strip()
-            number_view = e.find_all('span')[1].string.strip()
+            chapter_name = e.div.a.get_text()
+            
+            link_chapter = e.div.a.get('href')
+            time = e.find('div', class_="col-xs-4 no-wrap small text-center").get_text()
+            number_view = e.find('div', class_="col-xs-3 no-wrap small text-center").get_text()          
             list_chapters.append(Chapter(chapter_name, link_chapter, time, number_view))
         story.chapters = list_chapters
-        print(story)
 
         return story
     except requests.exceptions.RequestException as e:
         print(f"There is an error when access {e}")
 
+# crawl_list_chapters("vo-luyen-dinh-phong")
 
-crawl_list_chapters("vo-luyen-dinh-phong")
 @app.get("/api/v1/manga/truyen-tranh/{story_name}")
 async def get_chapter_list(story_name: str):
     if not story_name or story_name == "":
@@ -176,20 +205,25 @@ async def get_chapter_list(story_name: str):
     for ch in story.chapters:
         json_chapters.append(
             {
-                "chapterName": ch.chapter_name,
-                "link chapter": ch.link_chapter,
-                "number view" : ch.number_view,
-                "chapterDate": ch.time
+                "name": ch.chapter_name,
+                "link": ch.link_chapter,
+                "numberView" : ch.number_view,
+                "release": ch.time
             }
         )
     rest = {
-        "kind story": story.kind_story,
-        "manufacturer": story.manufacturer,
-        "description": story.dest,
+        "name": story.name,
+        "cover": story.cover,
+        "author": story.author,
+        "status": story.status,
+        "kind": story.kind,
+        "rating": float(story.rating),
+        "rating_count": story.rating_count,
+        # "manufacturer": "",
+        "description": "",
         "chapters": json_chapters
     }
     return rest
-
 
 def crawl_chapter_images(story_name: str, chapter_number: str) -> Optional[List[str]]:
     """ Scrape cac URL anh tu một chương truyện cụ thể"""
@@ -205,27 +239,32 @@ def crawl_chapter_images(story_name: str, chapter_number: str) -> Optional[List[
             return None
 
         soup = BeautifulSoup(response.content, 'html.parser')
-        elements = soup.find_all('img', class_ = re.compile('reading-img', re.IGNORECASE)) 
+        elements = soup.find_all('div', class_ = "page-chapter") 
+        # print(elements[1])
+        # return []
         
         if not elements:
             return []
 
         img_urls = []
         for e in elements:
-            img_url = e.get('src')
-            if img_url != None and img_url.startswith("https"):
+            img_url = e.img.get('data-src')
+            # print(img_url)
+            if img_url != None and (img_url.endswith("jpg") or img_url.endswith("webp")):
                 img_urls.append(img_url)
 
-            img_url = e.get('data-src')
-            if img_url != None and  img_url.startswith("https"):
-                img_urls.append(img_url)
+            # img_url = e.get('data-src')
+            # if img_url != None and  img_url.startswith("https"):
+            #     img_urls.append(img_url)
+        # print(img_urls)
         return img_urls
     except requests.exceptions.RequestException as e:
         print(f"There is an error when access {e}")
-        
-#define Endpoint API
+
+# urls = crawl_chapter_images("nguoi-choi-moi-cap-toi-da", "chuong-225")
+
+
 # /api/v1/manga/truyen-tranh/tuyet-the-vo-than/chapter-1076
-# @app.get("/api/v1/manga/chapter/{chapter_number}")
 @app.get("/api/v1/manga/truyen-tranh/{story_name}/{chapter_number}")
 async def get_chapter_data(story_name: str, chapter_number: str):
     """
@@ -241,12 +280,10 @@ async def get_chapter_data(story_name: str, chapter_number: str):
         raise HTTPException(status_code=404, detail=f"Not found chapter{chapter_number}")
 
     if not img_list:
-        raise HTTPException(status_code=404, detail=f"Found web but there is no image in chapter{chapter_number}, The stucture of HTML can be change")
+        raise HTTPException(status_code=404, detail=f"Found web but there is no image in {chapter_number}, The stucture of HTML can be change")
     return {
-        "chapter": chapter_number, 
-        "manga_name": "Võ Luyện Đỉnh Phong",
+        "manga_name": story_name,
+        "chapter_name": chapter_number, 
         "total_images": len(img_list),
         "images": img_list
     }
-
-
