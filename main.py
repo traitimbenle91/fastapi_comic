6d0597aec173459333a70a7e22613647e06616f8
tran manh <traitimbenle91@gmail.com>
2026-03-11 23:27:51 +0700

    test change agent

--- main.py
+++ main.py
@@ -13,7 +13,12 @@ app = FastAPI(
 
 #Header giúp giả lập trình duyệt để tránh bị chặn
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'vi-VN,vi;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Referer': 'https://www.google.com', # Giả vờ đến từ Google
+    'Connection': 'keep-alive',
 }
 
 root_url = "https://nettruyenviet1.com"
